082e172c04466c455c38a2ee3cd143f775c9e350
tabung <yurandarezky@atmaluhur.ac.id>
2024-11-21 11:32:15 +0700

    remove

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "I hope NixOS is my last stop. I'm really tired of this hopping around";
 
   inputs = {
-    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
