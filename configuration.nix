670012b23ab8b978eb822c055c2c13793c839600
Rezky Yuranda <yurandarezky@atmaluhur.ac.id>
2025-04-25 17:49:44 +0700

    flake home-manager modular

--- configuration.nix
+++ configuration.nix
@@ -100,45 +100,6 @@
     wget
   ];
 
-  ## Home-manager
-  home-manager.users.tabun = {pkgs, ...}: {
-    home.stateVersion = "22.11";  
-    home.packages = with pkgs; [
-      gnome-tweaks
-      gnomeExtensions.caffeine
-      gnomeExtensions.battery-health-charging
-
-    ];
-    programs.git = {
-      enable = true;
-      userEmail = "yurandarezky@atmaluhur.ac.id";
-      userName = "Rezky Yuranda";
-    }; 
-    # Dconf
-  dconf = {
-    enable = true;
-    settings = {
-      #"org/gnome/desktop/interface".color-scheme = "prefer-dark";
-        "org/gnome/mutter" = {
-          experimental-features = [ "scale-monitor-framebuffer"  "xwayland-native-scaling" ];
-        };
-        "org/gnome/shell" = {
-          disable-user-extensions = false;
-          enabled-extensions = with pkgs.gnomeExtensions; [
-            caffeine.extensionUuid
-            battery-health-charging.extensionUuid
-          ];
-        };
-
-        "org/gnome/desktop/interface" = {
-          icon-theme = "Tela-light";
-        };
-      };
-    };
-
-   
-  };
-
   system.stateVersion = "24.11"; # Did you read the comment?
   nix.settings.experimental-features = ["nix-command" "flakes"];
 }
--- home.nix
+++ home.nix
@@ -1,38 +1,60 @@
 { config, pkgs, ... }:
-{
-  ## Home-manager
-  home-manager.users.tabun = {pkgs, ...}: {
-    home.stateVersion = "22.11";  
-    home.packages = with pkgs; [
-      gnome-tweaks
-      gnomeExtensions.caffeine
-      gnomeExtensions.battery-health-charging
 
-    ];
-    programs.git = {
-      enable = true;
-      userEmail = "yurandarezky@atmaluhur.ac.id";
-      userName = "Rezky Yuranda";
-    }; 
-    # Dconf
+{
+  # import file
+  imports = [
+    ./apps/zsh.nix
+  ];
+  
+  ## Home-manager
+  home.stateVersion = "22.11";
+
+  # Home packages
+  home.packages = with pkgs; [
+    remmina
+    heroic
+    vscode
+
+    # Theme app
+    tela-icon-theme
+    gnome-tweaks
+    gnomeExtensions.caffeine
+    gnomeExtensions.battery-health-charging
+  ];
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userEmail = "yurandarezky@atmaluhur.ac.id";
+    userName = "Rezky Yuranda";
+  };
+
+  # Dconf settings
   dconf = {
     enable = true;
     settings = {
-      #"org/gnome/desktop/interface".color-scheme = "prefer-dark";
-        "org/gnome/mutter" = {
-          experimental-features = [ "scale-monitor-framebuffer"  "xwayland-native-scaling" ];
-        };
-        "org/gnome/shell" = {
-          disable-user-extensions = false;
-          enabled-extensions = with pkgs.gnomeExtensions; [
-            caffeine.extensionUuid
-            battery-health-charging.extensionUuid
-          ];
-        };
+      # Uncomment and set this if you want to enable the dark theme
+      # "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      # GNOME Mutter settings
+      "org/gnome/mutter" = {
+        experimental-features = [
+          "scale-monitor-framebuffer"
+          "xwayland-native-scaling"
+        ];
+      };
 
-        "org/gnome/desktop/interface" = {
-          icon-theme = "Tela-light";
-        };
+      # GNOME Shell settings
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          caffeine.extensionUuid
+          battery-health-charging.extensionUuid
+        ];
+      };
+
+      # GNOME Desktop Interface settings
+      "org/gnome/desktop/interface" = {
+        icon-theme = "Tela-light";
       };
     };
   };
