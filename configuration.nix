23ddf52437e2c54190e68b236912d2d7ae481cbd
Rezky Yuranda <yurandarezky@atmaluhur.ac.id>
2025-04-25 17:03:09 +0700

    add flake.nix & configuration

---
+++ configuration.nix
@@ -0,0 +1,146 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nix0s"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  
+  # Wiregurad
+  networking.wireguard.enable = true;
+
+  # Zram
+  zramSwap.enable = true;
+
+  # VGA
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [ amdvlk ];
+    extraPackages32 = with pkgs; [ driversi686Linux.amdvlk];
+  };
+
+
+  # Set your time zone.
+  time.timeZone = "Asia/Jakarta";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Sudo no password
+  security.sudo.wheelNeedsPassword = false;
+
+  # Allow unfree
+  nixpkgs.config.allowUnfree = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tabun = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "audio" "video" "disk" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+    ];
+  };
+
+  programs.firefox.enable = true;
+  programs.zsh.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+  ];
+
+  ## Home-manager
+  home-manager.users.tabun = {pkgs, ...}: {
+    home.stateVersion = "22.11";  
+    home.packages = with pkgs; [
+      gnome-tweaks
+      gnomeExtensions.caffeine
+      gnomeExtensions.battery-health-charging
+
+    ];
+    programs.git = {
+      enable = true;
+      userEmail = "yurandarezky@atmaluhur.ac.id";
+      userName = "Rezky Yuranda";
+    }; 
+    # Dconf
+  dconf = {
+    enable = true;
+    settings = {
+      #"org/gnome/desktop/interface".color-scheme = "prefer-dark";
+        "org/gnome/mutter" = {
+          experimental-features = [ "scale-monitor-framebuffer"  "xwayland-native-scaling" ];
+        };
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = with pkgs.gnomeExtensions; [
+            caffeine.extensionUuid
+            battery-health-charging.extensionUuid
+          ];
+        };
+
+        "org/gnome/desktop/interface" = {
+          icon-theme = "Tela-light";
+        };
+      };
+    };
+
+   
+  };
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+}
+
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "NixOS with Home Manager integrated into configuration.nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix  # Konfigurasi NixOS yang terintegrasi dengan Home Manager
+        ];
+      };
+    };
+  };
+}
+
